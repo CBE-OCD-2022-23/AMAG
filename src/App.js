e17960c6c5c80f0fa42b710e2e29d31f7bba1e44
Artovax <209798@student.pwr.edu.pl>
2022-11-22 08:48:25 +0100

    node_projekt

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Cześć Kocham Pieski 2!!!!!. Ta strona jest uruchamiana przez pipeline jenkinsa
+           na 13.73.148.81:8080  OCB </h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
